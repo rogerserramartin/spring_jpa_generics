a08602084b5291341af551fea5cf83e873f5a8b1
Roger Serra Martin <roger.serra92@gmail.com>
2022-01-16 22:35:48 +0100

    funciona

--- spring-jpa/spring-jpa/src/test/java/com/roger/springjpa/SpringJpaApplicationTests.java
+++ spring-jpa/spring-jpa/src/test/java/com/roger/springjpa/SpringJpaApplicationTests.java
@@ -1,13 +1,31 @@
 package com.roger.springjpa;
 
+import com.roger.springjpa.entity.Merchandise;
+import com.roger.springjpa.repository.MerchandiseRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootTest
 class SpringJpaApplicationTests {
 
+	@Autowired
+	ApplicationContext context;
+
 	@Test
-	void contextLoads() {
+	void prueba() {
+		MerchandiseRepository merchandiseRepository = context
+				.getBean(MerchandiseRepository.class);
+
+		Merchandise finalFantasy = Merchandise.builder()
+				.brand("Stranger of Paradise")
+				.price(70d)
+				.build();
+
+		merchandiseRepository.save(finalFantasy);
+		
 	}
 
 }
